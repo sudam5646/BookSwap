824b1fa13c2f5dcbe49c6c51fcde42989be29946
sudam5646 <61772723+sudam5646@users.noreply.github.com>
2021-01-29 22:53:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ app.use(require('./routes/book'))
 app.use(require('./routes/user'))
 
 if(process.env.NODE_ENV == "production"){
-    app.use(express.static('client/build'))
+    app.use(express.static('bookseller/build'))
     const path = require('path')
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html',))
+        res.sendFile(path.resolve(__dirname,'bookseller','build','index.html',))
     })
 }
 
