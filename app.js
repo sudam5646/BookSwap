0b46afb2a27460e0ae35c139c3f8d069bfcc79df
sudam5646 <61772723+sudam5646@users.noreply.github.com>
2021-01-29 22:44:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
-const {MONGOURI} = require('./key');
+const {MONGOURI} = require('./config/key');
 
 const PORT = process.env.PORT || 5000;
 
